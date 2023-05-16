0c5321bb1b19ab8c39dc3353a47fe2e5758e7eb4
Felipe Vilarinho <fvilarinho@gmail.com>
2023-05-16 13:32:55 -0300

    Update main.tf

--- iac/main.tf
+++ iac/main.tf
@@ -5,6 +5,7 @@ terraform {
     key                         = "akamai-vpc-demo.tfstate"
     region                      = "us-east-1"
     endpoint                    = "us-east-1.linodeobjects.com"
+    skip_region_validation      = true
     skip_credentials_validation = true
   }
   required_providers {
