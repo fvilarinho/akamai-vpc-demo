a82fb4d6d4bafc7a75dba96818be89b9b945a8b4
Felipe Vilarinho <fvilarinho@gmail.com>
2023-06-12 19:28:01 -0300

    Code improvements.

--- iac/vpcSiteToSite.tf
+++ iac/vpcSiteToSite.tf
@@ -13,22 +13,3 @@ resource "null_resource" "connectSite1ToSite2" {
 
   depends_on = [ null_resource.downloadVpnClient ]
 }
-
-# Connect the site 2 with site 1.
-resource "null_resource" "connectSite2ToSite1" {
-  provisioner "remote-exec" {
-    connection {
-      host        = linode_instance.vpcGatewaySite2.ip_address
-      user        = "root"
-      password    = random_password.vpcGateway.result
-      private_key = chomp(tls_private_key.vpc.private_key_openssh)
-    }
-
-    inline = [ "./connectToSite.sh ${linode_instance.vpcGatewaySite1.ip_address}" ]
-  }
-
-  depends_on = [
-    null_resource.downloadVpnClient,
-    null_resource.connectSite1ToSite2
-  ]
-}
