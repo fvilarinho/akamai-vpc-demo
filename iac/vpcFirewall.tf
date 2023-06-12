9f352270940b72d58a8e88379b76d653ea4716d1
Felipe Vilarinho <fvilarinho@gmail.com>
2023-06-12 19:47:43 -0300

    Code improvements.

--- iac/vpcFirewall.tf
+++ iac/vpcFirewall.tf
@@ -5,14 +5,6 @@ resource "linode_firewall" "vpcGatewaySite1" {
   outbound_policy = "ACCEPT"
 
   inbound {
-    label    = "allow-ssh"
-    action   = "ACCEPT"
-    protocol = "TCP"
-    ports    = "22"
-    ipv4     = [ "0.0.0.0/0" ]
-  }
-
-  inbound {
     label    = "allow-openvpn"
     action   = "ACCEPT"
     protocol = "UDP"
@@ -21,9 +13,7 @@ resource "linode_firewall" "vpcGatewaySite1" {
   }
 
   linodes    = [ linode_instance.vpcGatewaySite1.id ]
-  depends_on = [
-    linode_instance.vpcGatewaySite1
-  ]
+  depends_on = [ null_resource.connectSite1ToSite2 ]
 }
 
 # Defines the Firewall rules to access the VPC of site 2.
@@ -31,14 +21,6 @@ resource "linode_firewall" "vpcGatewaySite2" {
   label           = "${var.vpcGatewaySite2.id}-firewall"
   inbound_policy  = "DROP"
   outbound_policy = "ACCEPT"
-
-  inbound {
-    label    = "allow-ssh"
-    action   = "ACCEPT"
-    protocol = "TCP"
-    ports    = "22"
-    ipv4     = [ "${linode_instance.vpcGatewaySite1.ip_address}/32" ]
-  }
 
   inbound {
     label    = "allow-openvpn"
@@ -49,9 +31,7 @@ resource "linode_firewall" "vpcGatewaySite2" {
   }
 
   linodes    = [ linode_instance.vpcGatewaySite2.id ]
-  depends_on = [
-    linode_instance.vpcGatewaySite2
-  ]
+  depends_on = [ linode_instance.vpcGatewaySite2 ]
 }
 
 # Defines the Firewall rules to access the VPC nodes of the all sites.
